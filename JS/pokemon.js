c251105fe75e0feb89f6fdbf6fb22b90ee0c8a74
Andres-Soler <andres.solers@uniagustiniana.edu.co>
2025-09-25 21:31:06 +0000

    Pokemon

---
+++ JS/pokemon.js
@@ -0,0 +1,26 @@
+async function pokemon(id) {
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await res.json();
+
+        var root = document.getElementById("root");
+        let tipoPoke = data.types.map(type => `<span>${type.type.name}</span>`);
+
+        root.innerHTML = `
+        <section class="c-detalle">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="Imagen de ${data.name}" height="120" width="auto">
+            <p><strong>Nombre:</strong> ${data.name}</p>
+            <p><strong>ID:</strong> ${data.id}</p>
+            <p><strong>Altura:</strong> ${data.height / 10} m / <strong>Peso:</strong> ${data.weight / 10} kg</p>
+            <p><strong>HP:</strong> ${data.stats[0].base_stat}</p>
+            <p><strong>Velocidad:</strong> ${data.stats[5].base_stat}</p>
+            <p><strong>Ataque:</strong> ${data.stats[1].base_stat} / <strong>Defensa:</strong> ${data.stats[2].base_stat}</p>
+            <p><strong>Ataque Especial:</strong> ${data.stats[3].base_stat} / <strong>Defensa Especial:</strong> ${data.stats[4].base_stat}</p>
+            <p><strong>Tipos:</strong> ${tipoPoke}</p>
+        </section>
+        `;
+    } 
+    }
+}
+
+pokemon(9);
