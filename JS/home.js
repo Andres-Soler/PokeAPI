9f27a24393a84e8c9495ba1662b5c8d7a6ece697
Andres-Soler <andres.solers@uniagustiniana.edu.co>
2025-10-02 20:19:16 +0000

    cambios

--- JS/home.js
+++ JS/home.js
@@ -1,3 +1,53 @@
+function buscadorfunction(cualquiercosa){
+    console.log(cualquiercosa);
+}
+
 function home (){
     document.getElementById("root").innerHTML = "home"
+    //buscador
+    const buscador = document.createElement("input");
+    buscador.classList.add("c-buscador");
+    buscador.type = "text";
+    buscador.placeholder = "Buscar Pokémon...";
+    buscador.addEventListener("input", () => {
+            buscadorfuncion(buscador.value);
+    });
+
+    document.getElementById("root").appendChild(buscador);
+    
+    //contenedor filtro
+    const tipos = [
+        "normal", "fighting", "flying", "poison", "ground", "rock", "bug",
+        "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice",
+        "dragon", "dark", "fairy", "stellar", "unknown"
+    ];
+
+    function FiltroConexion(otracosa){
+        alert(otracosa)
+    }
+
+    const contenedorFiltro = document.createElement("section");
+    contenedorFiltro.classList.add("tipos-container"); 
+
+    for (let i = 0; i < tipos.length; i++) {
+        const btn = document.createElement("button");
+        btn.textContent = tipos[i];
+        
+        // Agregar el evento click para filtrar por tipo
+        btn.addEventListener("click", () => {
+            FiltroConexion(tipos[i]); 
+        });
+
+        // Agregar el botón al contenedor
+        contenedorFiltro.appendChild(btn);
+    }
+
+    //contenedor
+    const contenedorFiltro = document.createElement("section");
+    contenedorFiltro.classList.add("tipos-container");  // Esto es correcto
+
+
+    document.getElementById("root").appendChild(buscador);
+    document.getElementById("root").appendChild(cualquiercosa);
+    document.getElementById("root").appendChild(contenedorpokemones);
 }
